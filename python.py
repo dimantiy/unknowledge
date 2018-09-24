c518ce899d6b089951c780b7b0e0024fd9b09412
Dmitrii Pankov <dimantiy@users.noreply.github.com>
2018-09-24 10:58:11 +0500

    Update python.py

--- python.py
+++ python.py
@@ -6,3 +6,7 @@ datetime.datetime.now().isoformat()
 import sys
 sys.maxint   # Python 2
 sys.maxsize  # Python 3
+
+# Generate Exception
+raise Exception('Describe exception')
+raise SpecificException('Specific exception')
