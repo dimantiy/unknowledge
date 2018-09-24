5b81291de241f9260f0a75bfb026a966488e7c21
Dmitrii Pankov <dimantiy@users.noreply.github.com>
2018-09-24 12:53:59 +0500

    Python - Super

--- python.py
+++ python.py
@@ -10,3 +10,7 @@ sys.maxsize  # Python 3
 # Generate Exception
 raise Exception('Describe exception')
 raise SpecificException('Specific exception')
+
+# Call parent class's method
+super(Foo, self).foo(1, 2)  # Python 2
+super().foo(1, 2)           # Python 3
