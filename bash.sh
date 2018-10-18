d78b9d32b0f19a7f58940995b7d73f19649f8659
Dmitrii Pankov <dimantiy@users.noreply.github.com>
2018-10-18 09:25:01 +0500

    Bash - SSH & Loops

--- bash.sh
+++ bash.sh
@@ -6,3 +6,17 @@ echo -n "Enter Value: "
 read my_variable
 # OR
 read -p "Enter user: " user
+read -s "Enter pass: " password
+
+# SSH
+ssh-keygen
+ssh-agent
+ssh-add
+
+# Loops
+for VARIABLE in 1 2 3 4 5 .. N
+do
+	command1
+	command2
+	commandN
+done
